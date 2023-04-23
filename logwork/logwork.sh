c11080f2fb316bc65ed1997aef801a9df81852d8
Terry Brown <Brown.TerryN@epa.gov>
2023-04-23 09:45:45 -0500

    place cursor top of entry for h command

--- logwork/logwork.sh
+++ logwork/logwork.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "h" ] ; then
     echo >> ~/.worklog
     history | tail -n 30 | sed 's/^[[:space:][:digit:]]*//' >> ~/.worklog
-    vim -c "normal G" ~/.worklog
+    vim ~/.worklog -c "normal G" -c '?^\d\{8\}-\d\{4\}' -c "normal zz"
 else
     logwork.py $@
 fi
